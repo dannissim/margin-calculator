ba2df64eba0d7c3d69ee7ee79d9794dec0c2d8c7
Dan <nissim.dan@gmail.com>
2022-02-03 19:44:24 +0200

    finished

---
+++ margin_calculator.py
@@ -0,0 +1,20 @@
+REGULAR_MARGIN_REQUIREMENT = 0.25
+LEVERAGED_MARGIN_REQUIREMENT = 0.75
+
+
+def max_margin(leveraged_value, regular_value, percentage_of_leveraged_drop,
+               percentage_of_regular_drop, current_loan):
+    leveraged_value_after_drop = leveraged_value * (1 - percentage_of_leveraged_drop)
+    regular_value_after_drop = regular_value * (1 - percentage_of_regular_drop)
+    leveraged_margin_impact = (1 - REGULAR_MARGIN_REQUIREMENT) * leveraged_value_after_drop
+    regular_margin_impact = (1 - LEVERAGED_MARGIN_REQUIREMENT) * regular_value_after_drop
+    return (leveraged_margin_impact + regular_margin_impact -
+            current_loan) / (1 - (1 - REGULAR_MARGIN_REQUIREMENT) * percentage_of_regular_drop)
+
+
+def main():
+    return max_margin(20000, 10000, 0.6, 0.5, 0)
+
+
+if __name__ == '__main__':
+    print(main())
